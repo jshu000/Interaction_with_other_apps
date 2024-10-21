a4a4359b65e0eff2328c9277463f1d573a0419a3
Jshu <56064412+jshu000@users.noreply.github.com>
2024-10-21 23:38:38 +0530

    Map location sharing using intent to other apps
    Map location sharing using intent to other apps

--- app/src/main/java/com/example/interaction_with_other_apps/MainActivity.kt
+++ app/src/main/java/com/example/interaction_with_other_apps/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     lateinit var button:Button
+    lateinit var map:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +24,18 @@ class MainActivity : AppCompatActivity() {
                 Intent(Intent.ACTION_DIAL, number)
             }
             startActivity(callIntent)
+        }
+        map=findViewById<Button>(R.id.map)
+        map.setOnClickListener {
+            Toast.makeText(this,"Map Clicked",Toast.LENGTH_SHORT).show()
+            val mapIntent: Intent = Uri.parse(
+                "geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California"
+            ).let { location ->
+                // Or map point based on latitude/longitude
+                // val location: Uri = Uri.parse("geo:37.422219,-122.08364?z=14") // z param is zoom level
+                Intent(Intent.ACTION_VIEW, location)
+            }
+            startActivity(mapIntent)
         }
 
     }
